a28631767d11a2beab7e09f888872aa72be81421
najwadiraa <najwasteria@gmail.com>
2024-05-31 10:54:57 +0700

    jdsd

--- content/buku/aksi.php
+++ content/buku/aksi.php
@@ -2,8 +2,8 @@
 include "../../konfig/koneksi.php";
 
 if($_GET['act']=='simpan'){
-    mysqli_query($conn, "INSERT INTO tbmhs(nim, nama_mhs, pw) VALUES
-    ('$_POST[nim]', '$_POST[nama]', '$_POST[pw]')");
+    mysqli_query($conn, "INSERT INTO buku(id, judul, pencipta, deskripsi, thn_terbit) VALUES
+    ('$_POST[id]', '$_POST[judul]', '$_POST[pencipta]', , '$_POST[deskripsi]', '$_POST[thn_terbit]')");
 
     header("location: mahasiswa");
 }
--- content/buku/tambah.php
+++ content/buku/tambah.php
@@ -1,21 +1,33 @@
 <div class="container mt-3">
-        <form method="POST" action="mahasiswa-simpan">
+        <form method="POST" action="buku-simpan">
             <div class="row mb-3">
-                <label class="col-md-3 col-sm-3">NIM</label>
+                <label class="col-md-3 col-sm-3">id</label>
                 <div class="col-md-9 col-sm-9">
-                <input type="text" class="form-control" name="nim" required>
+                <input type="text" class="form-control" name="id" required>
             </div>
 </div>
             <div class="row mb-3">
-            <label class="col-md-3">Nama Mahasiswa</label>
+            <label class="col-md-3">Judul Buku</label>
                 <div class="col-md-9">
-                <input type="text" class="form-control" name="nama" required>
+                <input type="text" class="form-control" name="judul" required>
             </div>
         </div>
         <div class="row mb-3">
-            <label class="col-md-3">Password</label>
+            <label class="col-md-3">Penulis</label>
                 <div class="col-md-9">
-                <input type="text" class="form-control" name="pw">
+                <input type="text" class="form-control" name="pencipta">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-md-3">Deskripsi</label>
+                <div class="col-md-9">
+                <input type="text" class="form-control" name="deskripsi">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-md-3">Tahun Terbit</label>
+                <div class="col-md-9">
+                <input type="date" class="form-control" name="thn_terbit">
             </div>
         </div>
             <button type="submit" class="btn btn-success">SIMPAN</button>
